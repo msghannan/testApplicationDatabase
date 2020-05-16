74dd0a7e39fde39d770eb375131ac9708be25e66
M. Ghannan <msghannan@hotmail.com>
2020-05-16 14:05:33 +0200

    Skapade databasen med samtliga klasser och attribut, ej kopplingar

---
+++ TestApplicationDatabase/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApplicationDatabase.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentClass { get; set; }
+    }
+}
